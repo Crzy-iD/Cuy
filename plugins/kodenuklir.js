ede1a9ea26a6a298f85bd6ab815b03d4eb1a1b2c
Crzy-iD <113808384+Crzy-iD@users.noreply.github.com>
2022-09-22 22:48:05 +0700

    Update kodenuklir.js

--- plugins/kodenuklir.js
+++ plugins/kodenuklir.js
@@ -889,7 +889,7 @@ handler.tags = ['hentai']
 handler.command = /^kodenuklir$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
